61496850977b98a058935cef576c3502e06aeb1a
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-03-25 00:01:01 +0100

    yeet

--- kickstart20/roundA/workout.py
+++ kickstart20/roundA/workout.py
@@ -1,1 +1,20 @@
-#yeet
+from math import ceil
+
+testcases = int(input())
+
+for case in range(1, testcases+1):
+    n, k = [int(x) for x in input().split()]
+    times = [int(x) for x in input().split()]
+    dlower = 1 
+    dupper = max(times)
+    while dupper - dlower >= 1:
+        dopt = (dlower + dupper) // 2
+        ksum = 0
+        for i in range(n-1):
+            ki = ceil((times[i+1]-times[i])/dopt)-1
+            ksum += ki
+        if ksum >= k:
+            dlower = dopt
+        elif ksum < k:
+            dupper = dopt
+    print("Case #{}: {}".format(case, dopt))
