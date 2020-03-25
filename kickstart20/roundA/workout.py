e23e40142b4cc911b15c2c674168d9c91c61ef76
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-03-25 01:47:02 +0100

    felsök ASAP

--- kickstart20/roundA/workout.py
+++ kickstart20/roundA/workout.py
@@ -6,15 +6,15 @@ for case in range(1, testcases+1):
     n, k = [int(x) for x in input().split()]
     times = [int(x) for x in input().split()]
     dlower = 1 
-    dupper = max(times)
-    while dupper - dlower >= 1:
+    dupper = times[-1] - times[0]
+    while dupper - dlower > 1:
         dopt = (dlower + dupper) // 2
         ksum = 0
         for i in range(n-1):
             ki = ceil((times[i+1]-times[i])/dopt)-1
             ksum += ki
-        if ksum >= k:
-            dlower = dopt
-        elif ksum < k:
+        if ksum <= k:
             dupper = dopt
+        else:
+            dlower = dopt
     print("Case #{}: {}".format(case, dopt))
