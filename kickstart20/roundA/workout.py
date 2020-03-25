282ec56e79f5f41cb3468e9b776832230389e3af
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-03-25 19:15:58 +0100

    fan då

--- kickstart20/roundA/workout.py
+++ kickstart20/roundA/workout.py
@@ -5,16 +5,42 @@ testcases = int(input())
 for case in range(1, testcases+1):
     n, k = [int(x) for x in input().split()]
     times = [int(x) for x in input().split()]
-    dlower = 1 
+    dlower = 1
     dupper = times[-1] - times[0]
-    while dupper - dlower > 1:
+    ksum = 0
+    while dupper != dlower:
         dopt = (dlower + dupper) // 2
         ksum = 0
         for i in range(n-1):
             ki = ceil((times[i+1]-times[i])/dopt)-1
             ksum += ki
-        if ksum <= k:
+        if ksum < k:
             dupper = dopt
-        else:
-            dlower = dopt
-    print("Case #{}: {}".format(case, dopt))
+        elif ksum > k:
+            dlower = dopt +1
+        elif k == ksum:
+            break
+    while k == ksum:
+        dlast = dopt
+        dopt -= 1
+        ksum = 0
+        for i in range(n-1):
+            ki = ceil((times[i+1]-times[i])/dopt)-1
+            ksum += ki
+        
+        
+    # alts = [dopt - 1, dopt, dopt + 1]
+    # last = float("inf")
+    # for k in alts:
+    #     for i in range(n-1):
+    #         ki = ceil((times[i+1]-times[i])/dopt)-1
+    #         ksum += ki
+    #     if last <
+    # while k == ksum:
+    #     dlast = dopt
+    #     dopt -= 1
+    #     ksum = 0
+    #     for i in range(n-1):
+    #         ki = ceil((times[i+1]-times[i])/dopt)-1
+    #         ksum += ki
+    print("Case #{}: {}".format(case, dlast))#dlast))
