4679652aa37cd97c22017530d64bbefb9ef7b4fa
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-08-01 03:04:03 +0200

    some propositional logic

--- gsaultra20/doubling.py
+++ gsaultra20/doubling.py
@@ -1,3 +1,4 @@
+'''
 from math import log2
 
 def solution(a, m):
@@ -37,19 +38,26 @@ def solution(a, m):
     #     tot += pow(2, x, m)
     # return pow(tot, 1, m)
 #print(solution([6], 7))
-
 '''
+
+# def solution(a, m):
+#     tot = 0
+#     for mycket in a:
+#         mycket = pow(mycket, 1, (m-1))
+#         tot += pow(2, mycket, m)
+#     return pow(tot, 1, m)
+
 ### följande snippet ger 1s ###
 def solution(a, m):
-    lite = 0
+    lite = 500
     while 2**lite < m:
         lite += 1
+    #lite = len(str(bin(m)))-1
     res = pow(2, lite, m)
     tot = 0
     for mycket in a:
-        mycket = pow(mycket, 1, m-1) if mycket >= m else mycket
+        mycket = pow(mycket, 1, m-1) #if mycket >= m else mycket
         nåt = mycket//lite
-        rest = pow(mycket, 1, lite)
-        tot += pow(pow(res, nåt, m)*pow(2, rest, m), 1, m)
+        rest = mycket - nåt*lite
+        tot += pow(pow(res, nåt, m)*pow(2, rest, m), 1,m)
     return pow(tot, 1, m)
-'''
