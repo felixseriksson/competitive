5a3dd6878de54bffcfe3404468a15bec0b3bed81
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-09-14 15:36:26 +0200

    minor changes, nothing substantial

--- nwercqual20/explorerers.py
+++ nwercqual20/explorerers.py
@@ -5,10 +5,15 @@ for _ in range(cases):
     num = int(input())
     levels = Counter(sorted([int(x) for x in input().split()]))
     teams = 0
-    minn = min(levels.keys())
-    teams += levels[minn]
-    del levels[minn]
-    for val in levels:
-        maxx = max(levels.keys())
-        teams += floor(levels[maxx]/maxx)
-        levels[maxx] %= maxx
+    for val in levels.keys():
+        teams += floor(levels[val]/val)
+        levels[val] %= val
+
+    kvar = sorted(levels.elements())
+    team = []
+    while kvar:
+        last = kvar.pop(-1)
+        team.append(last)
+        if len(team) == last:
+            teams += 1
+            team = []
