0f7c2518b2e7e230953865f817e2cb638b777157
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2021-09-06 22:52:12 +0200

    more imc

--- IMC-64BIDS/round1/flatcallsandputs/flatcallsandputs.java
+++ IMC-64BIDS/round1/flatcallsandputs/flatcallsandputs.java
@@ -4,10 +4,10 @@ import java.lang.Math;
 
 class Flatcallsandputs {
     public static void main(String[] args) {
-        List<Integer> a = List.of(3, 9, 7);
-        List<Integer> b = List.of(1, 3, 2);
-        List<Integer> c = List.of(10, 10);
-        List<Integer> d = List.of(2, 2);
+        List<Integer> a = List.of(549, 170, 748, 1, 341, 270, 148, 672, 882, 427);
+        List<Integer> b = List.of(213, 313, 666, 896, 617, 430, 686, 135, 165, 148);
+        List<Integer> c = List.of(388, 282, 539, 769, 6, 860, 733, 964, 825, 422);
+        List<Integer> d = List.of(352, 476, 287, 22, 288, 327, 942, 846, 564, 324);
         int profit = calculateProfit(a, b, c, d);
         System.out.println(profit);
         // for (int bit = 0; bit < 10; bit++) {
@@ -19,45 +19,48 @@ class Flatcallsandputs {
         int ncallvolumes = callVolumes.size();
         int nputvolumes = putVolumes.size();
         int profit = 0;
-        for (int i = 0; i < (1 << ncallvolumes); i++) {
-            for (int j = 0; j < (1 << nputvolumes); j++) {
+        for (int i = 1; i < (1 << ncallvolumes); i++) {
+            for (int j = 1; j < (1 << nputvolumes); j++) {
                 int callvolsum = 0;
                 int putvolsum = 0;
-                for (int iindex = ncallvolumes; iindex > 0; iindex-- ) {
-                    // System.out.println("Trying to get index " + String.valueOf(ncallvolumes-iindex));
-                    callvolsum += getBit(i, iindex)*callVolumes.get(ncallvolumes-iindex);
+                int gains = 0;
+                int losses = 0;
+                
+                for (int calli = 0; calli < ncallvolumes; calli++) {
+                    if ((i & (1 << calli)) > 0) {
+                        callvolsum += callVolumes.get(calli);
+                    }
                 }
-                // System.out.println("Finished first indexing");
-                for (int jindex = nputvolumes; jindex > 0; jindex-- ) {
-                    // System.out.println("Trying to get index " + String.valueOf(nputvolumes-jindex));
-                    putvolsum += getBit(j, jindex)*putVolumes.get(nputvolumes-jindex);
+
+                for (int puti = 0; puti < nputvolumes; puti++) {
+                    if ((j & (1 << puti)) > 0) {
+                        putvolsum += putVolumes.get(puti);
+                    }
                 }
-                // System.out.println("Finished second indexing");
+                // System.out.println(callvolsum);
+                // System.out.println(putvolsum);
+                // System.out.println();
+
                 if (callvolsum == putvolsum) {
-                    int potentialloss = 0;
-                    int potentialgain = 0;
-                    for (int iindex = ncallvolumes; iindex > 0; iindex-- ) {
-                        // System.out.println("Adding callval at index " + String.valueOf(ncallvolumes-iindex));
-                        potentialloss += getBit(i, iindex)*callVolumes.get(ncallvolumes-iindex)*callStrikePrices.get(ncallvolumes-iindex);
+                    for (int puti = 0; puti < nputvolumes; puti++) {
+                        if ((j & (1 << puti)) > 0) {
+                            gains += putVolumes.get(puti)*putStrikePrices.get(puti);
+                        }
                     }
-                    // System.out.println("Finished third indexing");
-                    for (int jindex = nputvolumes; jindex > 0; jindex-- ) {
-                        // System.out.println("Adding putval at index " + String.valueOf(nputvolumes-jindex));
-                        potentialgain += getBit(j, jindex)*putVolumes.get(nputvolumes-jindex)*putStrikePrices.get(nputvolumes-jindex);
+                    System.out.println(gains);
+                    for (int calli = 0; calli < ncallvolumes; calli++) {
+                        if ((i & (1 << calli)) > 0) {
+                            losses += callVolumes.get(calli)*callStrikePrices.get(calli);
+                        }
                     }
-                    // System.out.println("Finished fourth indexing");
-                    System.out.println("Current i: " + String.valueOf(i));
-                    System.out.println("Current j: " + String.valueOf(j));
-                    int potentialprofit = potentialgain - potentialloss;
-                    System.out.println(potentialprofit);
-                    profit = (potentialprofit > profit) ? potentialprofit : profit;
+                    System.out.println(losses);
+
+                    if (gains - losses > profit) {
+                        profit = gains - losses;
+                    }
                 }
             }
         }
         return profit;
-    }
-
-    static int getBit(int n, int k) {
-        return (n >> k) & 1;
     }
 }
