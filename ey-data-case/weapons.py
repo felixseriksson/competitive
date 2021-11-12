f4a467969e7d0d149983ad2fed3f3818c9995c10
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2021-11-12 10:12:24 +0100

    nice

---
+++ ey-data-case/weapons.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+print(os.getcwd())
+
+weapons = pd.read_csv("Data/weapons.csv", sep=";")
+print(weapons.describe())
+# print(weapons.head())
+
+print("samples: \n")
+samples = pd.read_csv("Data/murderdata.csv", sep=";")
+print(samples.describe())
+# vettel = d1[]
+
+weaponnames = ["Revolver", "Knife"] # , "Lead Pipe"]# ["Revolver", "Knife", "Lead Pipe", "Wrench", "Candlestick"]
+for name in weaponnames:
+    tmp = weapons[weapons["Type"] == name]
+    print(name, end=": \n")
+    print(tmp.describe())
